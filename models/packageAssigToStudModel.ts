0632ca48d3a84e844eb7dc0a6dac8f0b67057ae1
@mi477048 <mi477048@gmail.com>
2024-04-16 10:49:26 -0300

    changes added

--- models/packageAssigToStudModel.ts
+++ models/packageAssigToStudModel.ts
@@ -2,20 +2,17 @@ import mongoose, { Model, Schema, Document } from 'mongoose';
 
 // Define an interface for the Lesson document
 interface packageAssigToStudInterface extends Document {
-	
 	instructor_id: mongoose.Types.ObjectId;
 	std_id: mongoose.Types.ObjectId;
-	package_id:mongoose.Types.ObjectId; 
+	package_id: mongoose.Types.ObjectId;
 	paymentPlan: string;
 	paymentType: string;
-	advance: number;
-	remainingAmount: number;
-	
+	advance: string;
+	remainingAmount: string;
 }
 
 const packageAssigToStudSchema = new Schema<packageAssigToStudInterface>(
 	{
-	
 		instructor_id: {
 			type: mongoose.Schema.Types.ObjectId,
 			ref: 'Instructor',
@@ -25,28 +22,28 @@ const packageAssigToStudSchema = new Schema<packageAssigToStudInterface>(
 			type: mongoose.Schema.Types.ObjectId,
 			ref: 'Student',
 		},
-	
+
 		package_id: {
-				type:mongoose.Schema.Types.ObjectId ,
-				ref: 'Package',
-				required: true,
-			},	
-        paymentPlan: {
-        	type:String,
-        	required: true,
-        },	
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Package',
+			required: true,
+		},
+		paymentPlan: {
+			type: String,
+			required: true,
+		},
 		paymentType: {
-        	type:String,
-        	required: true,
-        },	
+			type: String,
+			required: true,
+		},
 		advance: {
-			type:Number,
+			type: String,
 			required: true,
-		},	
+		},
 		remainingAmount: {
-			type:Number,
+			type: String,
 			required: true,
-		},					
+		},
 	},
 	{
 		timestamps: true, // Automatically adds createdAt and updatedAt fields
@@ -54,9 +51,10 @@ const packageAssigToStudSchema = new Schema<packageAssigToStudInterface>(
 );
 
 // Create the packageAssigToStudModel
-const packageAssigToStudModel: Model<packageAssigToStudInterface> = mongoose.model<packageAssigToStudInterface>(
-	'packageAssigToStud',
-	packageAssigToStudSchema
-);
-export {packageAssigToStudInterface};
+const packageAssigToStudModel: Model<packageAssigToStudInterface> =
+	mongoose.model<packageAssigToStudInterface>(
+		'packageAssigToStud',
+		packageAssigToStudSchema
+	);
+export { packageAssigToStudInterface };
 export default packageAssigToStudModel;
